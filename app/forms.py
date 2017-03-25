c6824ef84f26ce271b4aa80998c806b3caefc1ed
Александр <alstutor@gmail.com>
2017-03-25 20:20:02 +0700

    Adding the group list action is added.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,17 @@
+# coding: utf8
+
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class GroupForm(Form):
+    speciality = StringField(u'Наименование специальности', validators=[DataRequired()])
+    start_year = StringField(u'Год начала обучения', validators=[DataRequired()])
+    name = StringField(u'Наименование группы', validators=[DataRequired()])
+    students_list = TextAreaField()
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, lm, oid
-from forms import LoginForm
+from forms import *
 
 
 @app.route('/')
@@ -12,12 +12,21 @@ def index():
     return render_template("index.html", title=u"Добро пожаловать!")
 
 
+@app.route('/admin/group/new', methods=['GET', 'POST'])
+def admin_group_new():
+    form = GroupForm()
+    if form.validate_on_submit():
+        flash(u"Группа добавлена")
+        return redirect('/admin/group/list')
+    return render_template("new_group.html", title=u"Добро пожаловать!", form=form)
+
+
 @app.route('/test/start')
 def test_start():
     return render_template("test.html", title=u"Добро пожаловать!")
 
 
-@app.route('/login/',  methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 @oid.loginhandler
 def login():
     form = LoginForm()
